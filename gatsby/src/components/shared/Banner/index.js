62f9b717525f76d5fc1360c1f447c9f1841b24e0
Kevan Hannah <25513354+kevanhannah@users.noreply.github.com>
2022-07-20 11:31:19 -0400

    fix: remove problem file

--- gatsby/src/components/shared/Banner/index.js
+++ gatsby/src/components/shared/Banner/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-import { BannerStyles } from './Styles';
+import styled from 'styled-components';
+
+const BannerStyles = styled.aside`
+  width: 100%;
+  background-color: var(--primary-yellow);
+  border-bottom: 1px solid var(--light-grey);
+  text-align: center;
+  padding: 0.75em;
+
+  a {
+    font-size: 1em;
+    font-weight: 400;
+    color: var(--black);
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+      text-decoration-thickness: 0.125em;
+      text-underline-offset: 0.25em;
+      cursor: pointer;
+    }
+  }
+`;
 
 export default function Banner() {
   const {
