314ab60e7074abcc80c27b4b093c4a3ca67b9d10
Kevan Hannah <25513354+kevanhannah@users.noreply.github.com>
2022-08-01 08:54:58 -0400

    fix: bug fixes

--- gatsby/src/components/shared/Header/Nav/index.js
+++ gatsby/src/components/shared/Header/Nav/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { NavStyles } from './NavStyles';
+import { NavStyles } from './Styles';
 
 export default function Nav() {
   return (
