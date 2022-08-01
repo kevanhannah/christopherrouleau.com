e55803d7aeebb6656a0f93ed3517fe9cdb869871
Kevan Hannah <25513354+kevanhannah@users.noreply.github.com>
2022-08-01 09:01:58 -0400

    feat: mobile styles

--- gatsby/src/components/shared/Header/index.js
+++ gatsby/src/components/shared/Header/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import styled from 'styled-components';
 import Nav from './Nav';
 import Wordmark from '../Wordmark';
-
-const HeaderStyles = styled.header`
-  margin: 3em 0;
-  display: flex;
-  justify-content: space-between;
-`;
+import { HeaderStyles } from './Styles';
 
 export default function Header({ className }) {
   return (
     <HeaderStyles className={className}>
-      <Link to="/" style={{ minWidth: '25%' }}>
+      <Link to="/">
         <Wordmark />
       </Link>
       <Nav />
