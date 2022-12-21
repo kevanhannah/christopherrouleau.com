fa6e741baa69ec7911175448bd0ef7d4fee63958
Kevan Hannah <25513354+kevanhannah@users.noreply.github.com>
2022-12-21 11:15:23 -0500

    chore: update about page schema and structure

--- sanity/desk/about.js
+++ sanity/desk/about.js
@@ -1,10 +1,6 @@
 import { IoPersonOutline } from 'react-icons/io5';
 
 export default function about(S) {
-  // return S.listItem()
-  //   .title('About')
-  //   .schemaType('about')
-  //   .child(S.editor().title('About').schemaType('about').documentId('about'));
   return S.listItem()
     .title('About')
     .icon(IoPersonOutline)
