ba47ea78319b3edf3c867a2ef97e376f3ffe2827
Kevan Hannah <25513354+kevanhannah@users.noreply.github.com>
2023-03-24 19:51:06 -0400

    chore: update eslint config

--- sanity/.eslintrc.js
+++ sanity/.eslintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
   extends: ['wesbos'],
-  // rules: {
-  //   'import/no-unresolved': [
-  //     2,
-  //     {
-  //       ignore: ['^(all|part):'],
-  //     },
-  //   ],
-  // },
 };
